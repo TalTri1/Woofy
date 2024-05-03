c0e7b4a0256cfa0ba882201c29e046d45cc5ce6e
Naor Regev <naorregev5@gmail.com>
2024-05-03 12:38:19 +0300

    Saving in DB both customer and business

--- woofy-backend/src/main/java/com/woofy/woofy_backend/Models/Entities/BusinessEntities/BusinessEntity.java
+++ woofy-backend/src/main/java/com/woofy/woofy_backend/Models/Entities/BusinessEntities/BusinessEntity.java
@@ -28,17 +28,8 @@ public class BusinessEntity extends UserEntity {
     @Column(name = "business_name")
     private String businessName;
 
-    @Column(name = "address", nullable = false)
-    private String address;
-
-    @Column(name = "city", nullable = false)
-    private String city;
-
-    @Column(name = "zip_code", nullable = false)
-    private String zipCode;
-
     @Enumerated(EnumType.STRING)
-    @Column(name = "business_types", nullable = false)
+    @Column(name = "business_types")
     private List<BusinessTypeEnum> businessTypes;
 
     @Column(name = "about", length = 2000)
