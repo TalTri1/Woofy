af61c2c519581ad84bb9c97b057bd5f60b6a4e06
BEYOVAL <yuval1998@gmail.com>
2024-05-19 18:05:39 +0300

    Remove logo white background

--- woofy-frontend/src/components/logo/logo.jsx
+++ woofy-frontend/src/components/logo/logo.jsx
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import { useTheme } from '@mui/material/styles';
 import {RouterLink} from "../../routes/components";
-import woofyLogo from '/photo-in-ellipse@2x.png'
+import woofyLogo from '/public/assets/logo.png'
 
 // ----------------------------------------------------------------------
 
