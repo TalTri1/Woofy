009a349baf2f8e82885455c32a713d8c5dab63f4
nadavlior8 <nadav1997l@gmail.com>
2024-05-20 16:04:24 +0300

    log in and sign in pages redesign

--- woofy-frontend/src/Sections/LoginAndRegister/LoginView.tsx
+++ woofy-frontend/src/Sections/LoginAndRegister/LoginView.tsx
@@ -7,16 +7,18 @@ import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { useAuth } from "../../provider/AuthProvider";
 import { useRouter } from "../../routes/hooks";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
-import {Visibility, VisibilityOff} from "@mui/icons-material";
-import woofyLogo from '/public/assets/logo.png'
+import woofyLogo from '/public/assets/logo.png';
+import backgroundImage from '/public/assets/sign-in--modal--2@3x.png';
 
 export default function SignInComponent() {
     const theme = createTheme();
@@ -39,92 +41,161 @@ export default function SignInComponent() {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Grid container component="main" sx={{ height: '100vh' }}>
-                <CssBaseline />
-                <Grid
-                    item
-                    xs={false}
-                    sm={4}
-                    md={7}
-                    sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundColor: (t) =>
-                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
-                />
-                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                padding: 3,
+            }}
+        >
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="sm">
+                    <CssBaseline />
                     <Box
                         sx={{
-                            my: 8,
-                            mx: 4,
+                            marginTop: -25,
                             display: 'flex',
                             flexDirection: 'column',
                             alignItems: 'center',
+                            backgroundColor: 'rgba(255, 255, 255)',
+                            padding: 2.5,
+                            borderRadius: 2,
+                            maxWidth: '450px',
                         }}
                     >
-                        <Avatar src={woofyLogo} sx={{ m: 2, width: 100, height: 100 }} />
-                        <Typography component="h1" variant="h5">
+                        <Avatar src={woofyLogo} sx={{ width: 100, height: 100 }} />
+                        <Typography
+                            component="h1"
+                            sx={{
+                                fontSize: '36px',
+                                lineHeight: '120%',
+                                fontFamily: 'Inter',
+                                fontWeight: 'bold',
+                                color: 'black',
+                                textAlign: 'center',
+                                marginTop: '12px',
+                                marginBottom: '12px',
+                            }}
+                        >
                             Sign in
                         </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type={showPassword ? 'text' : 'password'}
-                                id="password"
-                                autoComplete="current-password"
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                onClick={handleClickShowPassword}
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
+                        <Typography
+                            component="h1"
+                            sx={{
+                                fontSize: '16px',
+                                lineHeight: '120%',
+                                fontFamily: 'Inter',
+                                fontWeight: 'regular',
+                                color: 'black',
+                                textAlign: 'center',
+                                marginBottom: '10px',
+                            }}
+                        >
+                            Welcome back! Sign in here.
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type={showPassword ? "text" : "password"}
+                                        id="password"
+                                        autoComplete="current-password"
+                                        InputProps={{
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <IconButton onClick={handleClickShowPassword}>
+                                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            ),
+                                        }}
+                                    />
+                                </Grid>
+                            </Grid>
                             <FormControlLabel
                                 control={<Checkbox value="remember" color="primary" />}
                                 label="Remember me"
+                                sx={{ mt: 2 }}
                             />
                             <Button
                                 type="submit"
                                 fullWidth
                                 variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
+                                sx={{
+                                    mt: 2.5,
+                                    mb: 3,
+                                    borderRadius: '30px',
+                                    backgroundColor: 'app1',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    py: 1,
+                                    px: 2,
+                                    fontSize: '16px',
+                                    fontFamily: 'Inter',
+                                    fontWeight: '600',
+                                    color: 'text-alternate',
+                                    textTransform: 'none',
+                                    border: 'none',
+                                }}
                             >
                                 Sign In
                             </Button>
-                            <Grid container>
+                            <Grid container justifyContent="center" sx={{ mt: 4 }}>
                                 <Grid item>
-                                    <Link href="/sign-up" variant="body2">
-                                        {"Don't have an account? Sign Up"}
+                                    <Typography
+                                        component="span"
+                                        sx={{
+                                            fontFamily: 'Inter',
+                                            fontSize: '16px',
+                                            fontWeight: '500',
+                                            color: 'black',
+                                            textDecoration: 'none',
+                                        }}
+                                    >
+                                        Don't have an account?
+                                    </Typography>
+                                    {' '}
+                                    <Link
+                                        href="/sign-up"
+                                        variant="body2"
+                                        sx={{
+                                            fontFamily: 'Inter',
+                                            fontSize: '16px',
+                                            fontWeight: 'bold',
+                                            color: 'app1',
+                                            textDecoration: 'none',
+                                        }}
+                                    >
+                                        Sign Up
                                     </Link>
                                 </Grid>
                             </Grid>
                         </Box>
                     </Box>
-                </Grid>
-            </Grid>
-        </ThemeProvider>
+                </Container>
+            </ThemeProvider>
+        </Box>
     );
 }
