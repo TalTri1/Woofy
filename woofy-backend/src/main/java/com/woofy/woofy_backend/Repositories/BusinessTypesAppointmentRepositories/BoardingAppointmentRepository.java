5a96e7a2bb4fff807892012dcc2c3c63ce938732
TalTri1 <tal.tri1999@gmail.com>
2024-06-09 20:34:30 +0300

    Adding delete option for day care, dog sitter and dog walker

--- woofy-backend/src/main/java/com/woofy/woofy_backend/Repositories/BusinessTypesAppointmentRepositories/BoardingAppointmentRepository.java
+++ woofy-backend/src/main/java/com/woofy/woofy_backend/Repositories/BusinessTypesAppointmentRepositories/BoardingAppointmentRepository.java
@@ -3,9 +3,12 @@ package com.woofy.woofy_backend.Repositories.BusinessTypesAppointmentRepositorie
 import com.woofy.woofy_backend.Models.Entities.AppointmentEntities.BusinessTypesAppointmentEntities.BoardingAppointmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BoardingAppointmentRepository extends JpaRepository<BoardingAppointmentEntity, Integer> {
     List<BoardingAppointmentEntity> findByBoardingEntity_Business_Id(Integer businessId);
     List<BoardingAppointmentEntity> findByDogId(Integer dogId);
+    List<BoardingAppointmentEntity> findAllByBoardingEntity_Business_IdAndDateBetween(Integer businessId, LocalDate startDate, LocalDate endDate);
+
 }
